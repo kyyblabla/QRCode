df2e26c39733f8e8474dabcefe0a681e120cf4bf
kyyblabla <kyyblabla@163.com>
2017-02-24 11:11:24 +0800

    finish 1.0

--- popup.js
+++ popup.js
@@ -1,5 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var currentUrl = chrome.extension.getBackgroundPage().currentUrl;
-    var qrcode = new QRCode(document.getElementById("qrcode"), currentUrl);
-    $("#input").val(currentUrl)
+    const QR_CODE_SIZE = 300;
+    var qrcode;
+    var urlInput = $("#url-input");
+    chrome.tabs.getSelected(null, function (tab) {
+        makeCode(tab.url);
+    })
+    $("#generate-but").click(function () {
+        makeCode(urlInput.val());
+    });
+
+    urlInput.keydown(function (e) {
+        if (e.keyCode == 13) {
+            makeCode(urlInput.val());
+        }
+    });
+
+    function makeCode(url) {
+        if (!url) {
+            return;
+        }
+        console.log(url)
+        if (!qrcode) {
+            qrcode = new QRCode($("#qrcode")[0], {
+                text: url,
+                width: QR_CODE_SIZE,
+                height: QR_CODE_SIZE
+            });
+        } else {
+            qrcode.clear();
+            qrcode.makeCode(url)
+        }
+        urlInput.val(url)
+    }
 });
